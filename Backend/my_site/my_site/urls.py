6456b059b8db1db8b161877d47666c6e321970d5
Trinadtsatyy <91557208+Trinadtsaty@users.noreply.github.com>
2022-07-01 17:43:47 +0300

    Update urls.py

--- Backend/my_site/my_site/urls.py
+++ Backend/my_site/my_site/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from forum.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+
+    path('forum/', index),
 ]
