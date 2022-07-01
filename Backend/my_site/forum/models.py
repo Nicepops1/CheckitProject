4a06316dc078069b0cd0d2ca4c92e1ba9f2aeaf6
Trinadtsatyy <91557208+Trinadtsaty@users.noreply.github.com>
2022-07-01 19:27:24 +0300

    Update models.py

--- Backend/my_site/forum/models.py
+++ Backend/my_site/forum/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Forum(models.Model):
+    avtorID = models.IntegerField()
+    teg = models.TextField()
+    title = models.CharField(max_length=150)
+    content = models.TextField()
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    creattime = models.DateTimeField(auto_now_add=True)
+    updatetime = models.DateTimeField(auto_now=True)
+
+class Otvet(models.Model):
+    avtorID=models.IntegerField()
+    postID=models.IntegerField()
+    content = models.TextField()
+    creattime = models.DateTimeField(auto_now_add=True)
+    updatetime = models.DateTimeField(auto_now=True)
