e9c7bdbe150cef5f5be0ad7ab5b45481174ce003
Trinadtsatyy <91557208+Trinadtsaty@users.noreply.github.com>
2022-07-04 17:20:49 +0300

    Update views.py

--- Backend/my_site/forum/views.py
+++ Backend/my_site/forum/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from .models import Forum
 from .models import Otvet
-
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib import messages
 
 def index(request):
     forumdate=Forum.objects.order_by('-creattime')
@@ -16,3 +17,19 @@ def index(request):
 
 def test(request):
     return HttpResponse('<h1> test 1')
+
+def register(request):
+    if request.method == 'POST':
+        form=UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Register-success')
+            return redirect('login')
+        else:
+            messages.error(request, 'Access denied')
+    else:
+        form=UserCreationForm()
+    return render(request, 'forum/register.html', {"form": form})
+
+def user_login(request):
+    return render(request, 'forum/login.html')
