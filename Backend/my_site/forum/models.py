04ab11dcc40e0c4d6ddf6ee3666df2675015e406
Trinadtsatyy <91557208+Trinadtsaty@users.noreply.github.com>
2022-07-04 12:59:11 +0300

    Update models.py

--- Backend/my_site/forum/models.py
+++ Backend/my_site/forum/models.py
@@ -4,7 +4,7 @@ class Forum(models.Model):
     avtorID = models.IntegerField()
     title = models.CharField(max_length=150)
     content = models.TextField()
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True, null=True)
     creattime = models.DateTimeField(auto_now_add=True)
     updatetime = models.DateTimeField(auto_now=True)
     category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
@@ -22,7 +22,8 @@ class Otvet(models.Model):
     content = models.TextField()
     creattime = models.DateTimeField(auto_now_add=True)
 
-  
+    def __str__ (self):
+        return self.content
 
 class Category(models.Model):
     title = models.CharField(max_length=150, db_index=True)
