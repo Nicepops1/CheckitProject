6f48c49964b61e04262532e152f821c525f2511a
Trinadtsatyy <91557208+Trinadtsaty@users.noreply.github.com>
2022-07-04 12:37:44 +0300

    Update models.py

--- Backend/my_site/forum/models.py
+++ Backend/my_site/forum/models.py
@@ -2,16 +2,30 @@ from django.db import models
 
 class Forum(models.Model):
     avtorID = models.IntegerField()
-    teg = models.TextField()
     title = models.CharField(max_length=150)
     content = models.TextField()
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     creattime = models.DateTimeField(auto_now_add=True)
     updatetime = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+
+    def __str__ (self):
+        return self.title
+   
+    class Meta:
+        verbose_name = 'mes'
+        
 
 class Otvet(models.Model):
     avtorID=models.IntegerField()
-    postID=models.IntegerField()
+    postID= models.ForeignKey(Forum, on_delete=models.CASCADE)
     content = models.TextField()
     creattime = models.DateTimeField(auto_now_add=True)
-    updatetime = models.DateTimeField(auto_now=True)
+
+  
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True)
+
+    def __str__ (self):
+        return self.title
