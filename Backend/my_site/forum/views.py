b8474bea8183c85bb37c54a3f23196c215d19120
Trinadtsatyy <91557208+Trinadtsaty@users.noreply.github.com>
2022-07-04 12:37:52 +0300

    Update views.py

--- Backend/my_site/forum/views.py
+++ Backend/my_site/forum/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Forum
+from .models import Otvet
+
+
 def index(request):
-    print(request)
-    return HttpResponse('Hello World')
+    forumdate=Forum.objects.order_by('-creattime')
+    otvetdate = Otvet.objects.order_by('-creattime')
+    context= {
+        'forum':forumdate, 
+        'otvet':otvetdate
+     }
+    return render(request, 'forum/index.html',context)
 
 def test(request):
     return HttpResponse('<h1> test 1')
