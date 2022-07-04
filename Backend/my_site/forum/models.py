d358d6ada83341a64fe4954d1a6393f973b05f64
Trinadtsatyy <91557208+Trinadtsaty@users.noreply.github.com>
2022-07-04 17:22:06 +0300

    Update models.py

--- Backend/my_site/forum/models.py
+++ Backend/my_site/forum/models.py
@@ -38,7 +38,8 @@ class Category(models.Model):
 
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,)
-    photo = models.ImageField(upload_to='users/%Y/%m/%d', blank=True)
+    photo = models.ImageField(upload_to='users/%Y/%m/%d', blank=True, null=True)
+    #нужно значение по умолчанию, это фото пользователей#
     content = models.TextField()
     city = models.CharField(max_length=150)
 
