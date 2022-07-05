edd57495d2b89dc34d8b5b14ce22e0febce88a5e
Trinadtsatyy <91557208+Trinadtsaty@users.noreply.github.com>
2022-07-05 16:35:55 +0300

    Update admin.py

--- Backend/my_site/forum/admin.py
+++ Backend/my_site/forum/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 
-from .models import Forum
-from .models import Otvet
+from .models import Post
+from .models import Coment
 from .models import Category
 from .models import Profile
 
-admin.site.register(Forum)
-admin.site.register(Otvet)
+admin.site.register(Post)
+admin.site.register(Coment)
 admin.site.register(Category)
 admin.site.register(Profile)
