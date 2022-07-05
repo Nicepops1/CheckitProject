d0d3b3bc98868a8f04371e1b02fc19f80c056e93
Trinadtsatyy <91557208+Trinadtsaty@users.noreply.github.com>
2022-07-05 16:36:10 +0300

    Update models.py

--- Backend/my_site/forum/models.py
+++ Backend/my_site/forum/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.conf import settings
 
-class Forum(models.Model):
+class Post(models.Model):
     avtorID = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, null=True)
     title = models.CharField(max_length=150)
     content = models.TextField()
@@ -9,6 +9,11 @@ class Forum(models.Model):
     creattime = models.DateTimeField(auto_now_add=True)
     updatetime = models.DateTimeField(auto_now=True)
     category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+    event = models.BooleanField(blank=True, null=True)
+    startdate = models.DateField(blank=True, null=True)
+    enddate = models.DateField(blank=True, null=True)
+    like= models.IntegerField(default=0)
+    unlike= models.IntegerField(default=0)
 
     def __str__ (self):
         return self.title
@@ -17,17 +22,19 @@ class Forum(models.Model):
         verbose_name = 'mes'
         
 
-class Otvet(models.Model):
+class Coment(models.Model):
     avtorID=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, null=True)
-    postID= models.ForeignKey(Forum, on_delete=models.CASCADE)
+    postID= models.ForeignKey(Post, on_delete=models.CASCADE)
     content = models.TextField()
     creattime = models.DateTimeField(auto_now_add=True)
+    photo = models.ImageField(upload_to='userphoto/%Y/%m/%d/', blank=True, null=True)
 
     def __str__ (self):
         return self.content
 
 class Category(models.Model):
-    title = models.CharField(max_length=150, db_index=True)
+    city = models.CharField(max_length=150, db_index=True)
+    
 
     def __str__ (self):
         return self.title
@@ -41,7 +48,8 @@ class Profile(models.Model):
     photo = models.ImageField(upload_to='users/%Y/%m/%d', blank=True, null=True)
     #нужно значение по умолчанию, это фото пользователей#
     content = models.TextField()
-    city = models.CharField(max_length=150)
+    city = models.CharField(max_length=150, blank=True, null=True)
+    rating = models.IntegerField(default=0)
 
     def __str__(self):
-        return 'Profile for user {}'.format(self.user.username)
+        return 'Profile for user {}'.format(self.user.usesname)
