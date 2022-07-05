f81d025443a2766b3f65d96029674af5a71844be
Trinadtsatyy <91557208+Trinadtsaty@users.noreply.github.com>
2022-07-05 16:59:01 +0300

    Update models.py

--- Backend/my_site/forum/models.py
+++ Backend/my_site/forum/models.py
@@ -8,18 +8,19 @@ class Post(models.Model):
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True, null=True)
     creattime = models.DateTimeField(auto_now_add=True)
     updatetime = models.DateTimeField(auto_now=True)
-    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+    city = models.ForeignKey('Citys', on_delete=models.PROTECT, null=True)
     event = models.BooleanField(blank=True, null=True)
     startdate = models.DateField(blank=True, null=True)
     enddate = models.DateField(blank=True, null=True)
     like= models.IntegerField(default=0)
     unlike= models.IntegerField(default=0)
+    publik = models.BooleanField(blank=True, default=False)
 
     def __str__ (self):
         return self.title
    
     class Meta:
-        verbose_name = 'mes'
+        verbose_name = 'Post'
         
 
 class Coment(models.Model):
@@ -32,12 +33,12 @@ class Coment(models.Model):
     def __str__ (self):
         return self.content
 
-class Category(models.Model):
+class Citys(models.Model):
     city = models.CharField(max_length=150, db_index=True)
     
 
     def __str__ (self):
-        return self.title
+        return self.city
 
 
 
@@ -46,10 +47,9 @@ class Category(models.Model):
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,)
     photo = models.ImageField(upload_to='users/%Y/%m/%d', blank=True, null=True)
-    #нужно значение по умолчанию, это фото пользователей#
-    content = models.TextField()
+    content = models.TextField(blank=True, null=True)
     city = models.CharField(max_length=150, blank=True, null=True)
     rating = models.IntegerField(default=0)
 
     def __str__(self):
-        return 'Profile for user {}'.format(self.user.usesname)
+        return 'Profile for user {}'.format(self.user.username)
