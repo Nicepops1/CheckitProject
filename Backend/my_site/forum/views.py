848fb301d1aecbe84748c3430f94039b82cec894
Trinadtsatyy <91557208+Trinadtsaty@users.noreply.github.com>
2022-07-07 15:32:57 +0300

    Update views.py

--- Backend/my_site/forum/views.py
+++ Backend/my_site/forum/views.py
@@ -10,26 +10,32 @@ def index(request):
     forumdate=Post.objects.order_by('-creattime')
     otvetdate = Coment.objects.order_by('-creattime')
     context= {
-        'forum':forumdate, 
-        'otvet':otvetdate
+        'Post':forumdate, 
+        'Coment':otvetdate
      }
     return render(request, 'forum/index.html',context)
+def map(request):
+    PostMark=Post.objects.order_by('creattime')
+    context1= {
+        'Mark':PostMark
+     }
+    return render(request, 'map/index.html', context1)
 
 def test(request):
     return HttpResponse('<h1> test 1')
 
 def register(request):
     if request.method == 'POST':
-        form=UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
+        form1=UserCreationForm(request.POST)
+        if form1.is_valid():
+            form1.save()
             messages.success(request, 'Register-success')
             return redirect('login')
         else:
             messages.error(request, 'Access denied')
     else:
-        form=UserCreationForm()
-    return render(request, 'forum/register.html', {"form": form})
+        form1=UserCreationForm()
+    return render(request, 'forum/register.html', {"form1": form1})
 
 def user_login(request):
     return render(request, 'forum/login.html')
