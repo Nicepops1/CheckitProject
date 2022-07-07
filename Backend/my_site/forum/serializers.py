c2a06bbaf7d9b21f60f417a8cd0493e881bfd7c3
Trinadtsatyy <91557208+Trinadtsaty@users.noreply.github.com>
2022-07-07 15:32:44 +0300

    Create serializers.py

---
+++ Backend/my_site/forum/serializers.py
@@ -0,0 +1,25 @@
+from dataclasses import fields
+from rest_framework import serializers
+from .models import *
+from django.db import models
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= Post
+        fields = '__all__'
+
+class ComentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= Coment
+        fields = '__all__'
+
+class CitysSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= Citys
+        fields = '__all__'
+
+class ProfilSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= Profile
+        fields = '__all__'
+
