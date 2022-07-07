dc2976d0d2d4756e5ca681a29c4f1b65b8cd15a2
Trinadtsatyy <91557208+Trinadtsaty@users.noreply.github.com>
2022-07-07 15:32:55 +0300

    Update urls.py

--- Backend/my_site/forum/urls.py
+++ Backend/my_site/forum/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
+from rest_framework import routers
+from .api import *
+
 from .views import *
+
+router = routers.DefaultRouter()
+router.register('api/post',PostViewSet, 'post')
+router.register('api/coment',ComentViewSet, 'coment')
+router.register('api/citys',CitysViewSet, 'citys')
+router.register('api/profile',ProfileViewSet, 'profile')
+
+urlpatterns = router.urls
 urlpatterns= [
     path('',index),
     path('register/',register, name='register'),
     path('login/',user_login, name='login'),
+    path('map/', map, name='map'),
 ]
-
-
-
