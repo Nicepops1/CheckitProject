84d29b60ca2e49f8e6dbade13932493b6d45b8da
Trinadtsatyy <91557208+Trinadtsaty@users.noreply.github.com>
2022-07-13 13:28:28 +0300

    Update mark.js

--- Backend/my_site/forum/templates/map/js/mark.js
+++ Backend/my_site/forum/templates/map/js/mark.js
@@ -1,11 +1,21 @@
-ymaps.ready(init);
-
-function init() {
-    var myMap = new ymaps.Map('map', {
-        center: [55.76, 37.64],
-        zoom: 10
-    }, {
-        searchControlProvider: 'yandex#search'
-    }),
-
-}
+const map = new mapgl.Map('container', {
+    center: [37.64, 55.76],
+    zoom: 8,
+    key: '123',
+    style: 'c080bb6a-8134-4993-93a1-5b4d8c36a59b'
+});
+const marker = new mapgl.Marker(map, {
+    coordinates: [38.64, 55.76],
+    icon: 'https://docs.2gis.com/img/mapgl/marker.svg',
+});
+const tooltipEl = document.getElementById('tooltip');
+marker.on('mouseover', (event) => {
+    // Offset in pixels
+    const offset = 5;
+    tooltipEl.style.top = `${event.point[1] + offset}px`;
+    tooltipEl.style.left = `${event.point[0] + offset}px`;
+    tooltipEl.style.display = 'block';
+});
+marker.on('mouseout', (e) => {
+    tooltipEl.style.display = 'none';
+});
